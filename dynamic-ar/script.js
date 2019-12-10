863aae301517dd9fa2a33a511907de2d35d5033c
jamesadkins <jamesadkins7117@gmail.com>
2019-12-10 09:07:33 +0530

    Update script.js

--- dynamic-ar/script.js
+++ dynamic-ar/script.js
@@ -1,3 +1,27 @@
+const clickListener = function (ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
+
+    const name = ev.target.getAttribute('name');
+    const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+    if (el && el === ev.target) {
+        // after click, we are adding a label with the name of the place
+        const label = document.createElement('span');
+        const container = document.createElement('div');
+        container.setAttribute('id', 'place-label');
+        label.innerText = name;
+        container.appendChild(label);
+        document.body.appendChild(container);
+
+        setTimeout(() => {
+            // that will disappear after less than 2 seconds
+            container.parentElement.removeChild(container);
+        }, 1500);
+     }
+ };
+
+
 // getting places from APIs
 function loadPlaces(position) {
     const params = {
